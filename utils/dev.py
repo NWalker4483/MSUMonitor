c1714cab62c99ff83507be734f6cbadb4b85766b
Nile Walker <niwal7@morgan.edu>
2020-11-16 12:41:14 -0500

    Fixed Dynamic Dropdown updates for when subj was changed but crn wasnt

--- utils/dev.py
+++ utils/dev.py
@@ -1,11 +1,12 @@
 
 def Clear(manager, cmd_str):
-    print(len(manager.getStudentNames()))
+    num = len(manager.getStudentNames())
     names = manager.getStudentNames().copy()
     for name in names:
         manager.RemoveStudent(name)
     
     print(len(manager.getStudentNames()))
+    return f"Removed all {num} students from the system"
 cmd_set = {
     "clear": Clear
 }
