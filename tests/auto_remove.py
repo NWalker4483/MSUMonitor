126d5c3c8649b68d3f4a840db050df57e371b61b
Nile Walker <niwal7@morgan.edu>
2020-11-20 14:30:15 -0500

    FIXED auto_remove test

--- tests/auto_remove.py
+++ tests/auto_remove.py
@@ -5,7 +5,10 @@ sys.path.insert(0, parent_dir)
 from manager import Manager
 from student import Student
 manager = Manager()
-manager.AddStudent(Student("niwal7", None))
+student = Student("niwal7", None)
+manager.AddStudent(student)
 manager.AddCourseSubscribtion("202070","TEST","101","111111","niwal7")
-for _ in range(4):
+for _ in range(5):
     manager.CheckCourseAvailability()
+#manager.RemoveCourseSubscribtion("202070","TEST","101","111111","niwal7")    
+assert(len(student.getNeededCourses())==0)
